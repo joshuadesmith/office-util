0cb46972151b3f581a5162a2e9163fb3682e6c67
Joshua Smith <joshuadesmith@utexas.edu>
2018-01-31 09:37:17 -0600

    Initial commit; added basic README, .gitignore, and empty officeutil.py

---
+++ officeutil.py
@@ -0,0 +1,13 @@
+# officeutil.py
+# Written by Josh Smith
+# Contains some scripts for automating intern work
+
+# Script ideas:
+#	Generate general coworker affidavits from base PDF and Excel containing file names
+#	Move executed files (affidavits and releases) to client files [implemented in filemove.py]
+#	Move drafted affidavits to client files [extend filemove.py]
+
+
+# Main Module
+if __name__ == "__main__":
+	print('TODO: Implement')
