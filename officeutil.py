49a0f650619da322fcbd06eb306739a714f5a23d
Joshua Smith <joshuadesmith@utexas.edu>
2018-01-31 12:56:21 -0600

    running officeutil.py now makes copies of base.pdf with names specified in filelisting.txt

--- officeutil.py
+++ officeutil.py
@@ -7,7 +7,43 @@
 #	Move executed files (affidavits and releases) to client files [implemented in filemove.py]
 #	Move drafted affidavits to client files [extend filemove.py]
 
+import shutil, os
+
+wdir = 'C:\\Users\\joshua\\Desktop\\'
+fnl = 'filelisting.txt'
+ftbc = 'base.pdf'
+dest = 'Quigley\\'
+
+
+# Get file paths for fnl and ftbc
+def get_filepaths():
+	base = wdir + ftbc
+	listing = wdir + fnl
+	return base, listing
+
+
+# Get path to destination folder
+def get_dest_path():
+	return wdir + dest
+
+
+# Generates copies of the specified PDF with names in array
+def generate_copies(file_to_copy, file_names):
+	dest_path = get_dest_path()
+	for name in file_names:
+		dest = dest_path + name.strip() + '.pdf'
+		shutil.copy(file_to_copy, dest)
+
+
+# Generates documents given TEXT FILE with file name listing
+def generate_documents(base_file, text_file):
+	names = open(text_file, mode='r', encoding='utf-8').readlines()
+	generate_copies(base_file, names)
+
 
 # Main Module
 if __name__ == "__main__":
-	print('TODO: Implement')
+	print('Generating copies of base.pdf')
+	base, listing = get_filepaths()
+	generate_documents(base, listing)
+	print('Process complete')
