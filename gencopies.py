216b0ebd0a9391cf8c8acd450200614f4cc3a85f
Joshua Smith <joshuadesmith@utexas.edu>
2018-02-01 10:43:10 -0600

    added check for resource files in gencopies.py

--- gencopies.py
+++ gencopies.py
@@ -10,7 +10,6 @@
 
 import shutil, os
 
-wdir = os.path.dirname(os.path.realpath(__name__))
 listing_txt = os.path.realpath('filelisting.txt')
 base_pdf = os.path.realpath('base.pdf')
 copy_dest = os.path.realpath('copies')
@@ -20,12 +19,32 @@ copy_dest = os.path.realpath('copies')
 def generate_copies():
 	names = open(listing_txt, mode='r', encoding='utf-8').read().splitlines()
 	for name in names:
-		dest = copy_dest + '\\' + name + '.pdf'
+		dest = copy_dest + '\\' + name
 		shutil.copy(base_pdf, dest)
+
+
+# Check whether or not the required files are in the working directory
+def check_resources():
+	wdir = os.path.dirname(os.path.realpath(__name__))
+	bp = wdir + '\\base.pdf'
+	fl = wdir + '\\filelisting.txt'
+	cp = wdir + '\\copies'
+	clear = True
+	if not os.path.exists(bp):
+		print("ERROR: base.pdf not found")
+		clear = False
+	if not os.path.exists(fl):
+		print("ERROR: filelisting.txt not found")
+		clear = False
+	if not os.path.exists(cp):
+		print("ERROR: copies directory not found")
+		clear = False
+	return clear
 
 
 # Main Module
 if __name__ == "__main__":
-	print('Generating copies...')
-	generate_copies()
-	print('Complete')
+	if check_resources():
+		print('Generating copies...')
+		generate_copies()
+		print('Complete')
