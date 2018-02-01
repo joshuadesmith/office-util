69406d7c6d69a4787a9ab32962381a39555c8455
Joshua Smith <joshuadesmith@utexas.edu>
2018-02-01 09:23:14 -0600

    changed name of officeutil.py to gencopies.py

--- gencopies.py
+++ gencopies.py
@@ -1,11 +1,6 @@
-# officeutil.py
+# gencopies.py
 # Written by Josh Smith
-# Contains some scripts for automating intern work
-
-# Script ideas:
-#	Generate general coworker affidavits from base PDF and Excel containing file names
-#	Move executed files (affidavits and releases) to client files [implemented in filemove.py]
-#	Move drafted affidavits to client files [extend filemove.py]
+# Creates copies of a pdf file
 
 import shutil, os
 
